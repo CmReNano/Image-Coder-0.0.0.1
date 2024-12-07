6eaf2d88509578c1248c677ffc94973304a1df36
CmReNano <danilkarasenko86@gmail.com>
2024-12-07 12:46:27 +0200

    Add files via upload

---
+++ Image Coder 0.0.0.1/DeBug/Updata.level.py
@@ -0,0 +1,15 @@
+Level_0_0_0_2 = {
+    """
+    [Moresymbols] (min 40)
+    [More words] (min 2)
+    """
+}
+
+Level_1_0_0_0 = {
+    """
+    [Custom Program]
+    [More symbols] (min 75)
+    [More words] (min 15)
+    [More languages] (min 10)
+    """
+}
